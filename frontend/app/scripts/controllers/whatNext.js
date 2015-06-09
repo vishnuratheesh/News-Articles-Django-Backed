5c28ded89cb916e6723a2b7ed3c5a2e2e797116b
Vishnu Ratheesh <vishnu@levitum.com>
2015-06-09 22:59:30 +0530

    Fix for what next scroll. Smooth now.

--- frontend/app/scripts/controllers/whatNext.js
+++ frontend/app/scripts/controllers/whatNext.js
@@ -14,12 +14,31 @@ angular.module('frontendApp').directive('whatnext', function() {
     },
     templateUrl: 'views/whatNext.html'
   };
-}).controller('WhatNextSectionCtrl', function($scope, getArticles) {
+}).controller('WhatNextSectionCtrl', function($scope, $timeout, getArticles) {
   $scope.whatnext = [{
     'title': 'Awesome stuff coming up! Soon...'
   }];
 
   getArticles.getJson().then(function(data) {
     $scope.whatnext = data.results;
+
+    var letsSlickIt = function() {
+      $('.articleslider').slick({
+        infinite: false,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        arrows: false
+      });
+      $('#slickNext').click(function() {
+        $("#articleslider").slick('slickNext');
+      });
+      $('#slickPrev').click(function() {
+        $("#articleslider").slick('slickPrev');
+      });
+
+
+    };
+    $timeout(letsSlickIt, 200);
+
   });
 });
