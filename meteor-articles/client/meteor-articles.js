2fc6fd969347a3d166d6dc5e95f49c74b8c4750f
Vishnu Ratheesh <vishnu@levitum.com>
2015-06-12 17:36:58 +0530

    Left Nav and Slick carousel What next integrated.

--- meteor-articles/client/meteor-articles.js
+++ meteor-articles/client/meteor-articles.js
@@ -1,17 +1,96 @@
 console.log("only on client")
 
-// counter starts at 0
-Session.setDefault('counter', 0);
+Articles = new Mongo.Collection("articles");
 
-Template.hello.helpers({
-  counter: function() {
-    return Session.get('counter');
-  }
+Router.map(function() {
+    this.route('about'); // By default, path = '/about', template = 'about'
+    this.route('home', {
+        path: '/', //overrides the default '/home'
+    });
+    this.route('articles', {
+        data: function() {
+                return Articles.find()
+            } //set template data context
+    });
+    this.route('article', {
+        path: '/article/:id',
+        data: function() {
+            return Articles.findOne({
+                id: parseInt(this.params.id)
+            })
+        },
+        template: 'fullArticle'
+    });
 });
 
-Template.hello.events({
-  'click button': function() {
-    // increment the counter when button is clicked
-    Session.set('counter', Session.get('counter') + 1);
-  }
+//
+Template.previewarticles.rendered = function() {
+    $('#carousel').slick({
+        dots: false,
+        arrows: false,
+        infinite: false,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+    });
+}
+
+Template.registerHelper('shortTitle', function(passedString) {
+    var fooText = passedString.substring(0, 20); //same as truncate.
+    return new Spacebars.SafeString(fooText)
+});
+
+Template.whatnext.helpers({
+    articles: function() {
+        return Articles.find({});
+    }
+});
+
+Template.whatnext.events({
+    "click .nextArticles": function() {
+        //alert("nextArticles");
+        $('#carousel').slick('slickNext');
+    },
+    "click .prevArticles": function() {
+        //alert("prevArticles");
+        $('#carousel').slick('slickPrev');
+    }
+
+    // $('#slickNext').click(function() {
+    //       
+    //     });
+    //     $('#slickPrev').click(function() {
+    //       $('#articleslider').slick('slickPrev');
+    //     });
+});
+
+// Template menu Helper
+Template.menu.helpers({
+    menus: function() {
+        return {
+            'current': 'index',
+            'left': [{
+                'active': true,
+                'link': '/',
+                'text': 'Nav 1'
+            }, {
+                'active': false,
+                'link': '/',
+                'text': 'Nav 2'
+            }, {
+                'active': false,
+                'link': '/',
+                'text': 'Nav 3'
+            }, {
+                'active': false,
+                'link': '/',
+                'text': 'Nav 4'
+            }, {
+                'active': false,
+                'link': '/',
+                'text': 'Nav 5'
+            }],
+            'rightLink': '\/session\/index',
+            'rightText': 'Log In'
+        };
+    }
 });
--- meteor-articles/server/meteor-articles.js
+++ meteor-articles/server/meteor-articles.js
@@ -1,4 +1,29 @@
 Meteor.startup(function() {
-  // code to run on server at startup
+  var apiBaseURL = "http://127.0.0.1:5000";
+  Articles = new Mongo.Collection("articles");
   console.log("only on server")
+
+  Meteor.http.get(apiBaseURL + "/articles/?format=json", function(error, result) {
+    if (error) {
+      console.log('http get FAILED!');
+    } else {
+      console.log('http get SUCCES');
+      if (result.statusCode === 200) {
+        console.log('Status code = 200!');
+        //console.log(result.content);
+        //Articles.remove({});
+
+        var res = JSON.parse(result.content)
+        res.results.forEach(function(article) {
+          // Articles.remove({
+          //   id: article.id
+          // });
+          //Articles.insert(article);
+          console.log("Inserted: " + article.id);
+        });
+
+      }
+    }
+  });
+
 });
