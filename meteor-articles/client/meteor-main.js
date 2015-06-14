1e10d72c752e4e92d54ae8e0e444ab6f842d19d7
Vishnu Ratheesh <vishnu@levitum.com>
2015-06-14 12:20:35 +0530

    Use methods to return JSON for part 2 of the problem.

--- meteor-articles/client/meteor-main.js
+++ meteor-articles/client/meteor-main.js
@@ -2,49 +2,47 @@ var apiBaseURL = "http://127.0.0.1:5000";
 
 Articles = new Mongo.Collection("articles");
 Articles.initEasySearch(['title', 'id'], {
-  'use' : 'mongo-db'
+  'use': 'mongo-db'
 });
 
 Meteor.subscribe("articles");
 
 Router.map(function() {
-    this.route('search',{
-      path: '/search',
-    }); // By default, path = '/about', template = 'about'
-    this.route('home', {
-        path: '/', //overrides the default '/home'
-    });
-    this.route('articles', {
-        data: function() {
-                return Articles.find()
-            } //set template data context
-    });
-    // this.route('article', {
-    //     path: '/article/:id',
-    //     data: function() {
-    //         return Articles.findOne({
-    //             id: parseInt(this.params.id)
-    //         })
-    //     },
-    //     template: 'fullArticle'
-    // });
-    this.route('article', {
-    path: '/article/:id',
+  this.route('search', {
+    path: '/search',
+  }); // By default, path = '/about', template = 'about'
+  this.route('home', {
+    path: '/', //overrides the default '/home'
+  });
+  this.route('articles', {
     data: function() {
+      return Articles.find()
+    } //set template data context
+  });
+  // this.route('article', {
+  //     path: '/article/:id',
+  //     data: function() {
+  //         return Articles.findOne({
+  //             id: parseInt(this.params.id)
+  //         })
+  //     },
+  //     template: 'fullArticle'
+  // });
+  this.route('article', {
+    path: '/article/:id',
+    waitOn: function() {
 
-      Meteor.http.get(apiBaseURL + "/articles/"+this.params.id+"/?format=json", function(error, result) {
-        if (error) {
-          console.log('http get FAILED!');
-        } else {
-          console.log('http get SUCCESS');
-          if (result.statusCode === 200) {
-            //this.response.writeHead(200, {'Content-Type': 'application/json'});
-            //this.response.end(JSON.stringify(result.content));
-            Session.set("jsondata", result.content);
-            return {'data': 2};
-          }
+      console.log("about to call");
+
+      Meteor.call("getArticleJSON", this.params.id, function(err, response) {
+        console.log("call returned");
+        if (err) {
+          Session.set('serverDataResponse', "Error:" + err.reason);
+          return;
         }
+        Session.set("jsondata", response.content);
       });
+
     },
     template: 'jsonArticle'
   });
@@ -52,114 +50,115 @@ Router.map(function() {
 
 //
 Template.previewarticles.rendered = function() {
-    $('#carousel').slick({
-        dots: false,
-        arrows: false,
-        infinite: false,
-        slidesToShow: 4,
-        slidesToScroll: 4,
-    });
+  $('#carousel').slick({
+    dots: false,
+    arrows: false,
+    infinite: false,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+  });
 }
 
 Template.registerHelper('shortTitle', function(passedString) {
-    var fooText = passedString.substring(0, 20); //same as truncate.
-    return new Spacebars.SafeString(fooText)
+  var fooText = passedString.substring(0, 20); //same as truncate.
+  return new Spacebars.SafeString(fooText)
 });
 
 Template.whatnext.helpers({
-    articles: function() {
-        return Articles.find({});
-    }
+  articles: function() {
+    return Articles.find({});
+  }
 });
 
 Template.whatnext.events({
-    "click .nextArticles": function() {
-        //alert("nextArticles");
-        $('#carousel').slick('slickNext');
-    },
-    "click .prevArticles": function() {
-        //alert("prevArticles");
-        $('#carousel').slick('slickPrev');
-    }
+  "click .nextArticles": function() {
+    //alert("nextArticles");
+    $('#carousel').slick('slickNext');
+  },
+  "click .prevArticles": function() {
+    //alert("prevArticles");
+    $('#carousel').slick('slickPrev');
+  }
 
-    // $('#slickNext').click(function() {
-    //       
-    //     });
-    //     $('#slickPrev').click(function() {
-    //       $('#articleslider').slick('slickPrev');
-    //     });
+  // $('#slickNext').click(function() {
+  //       
+  //     });
+  //     $('#slickPrev').click(function() {
+  //       $('#articleslider').slick('slickPrev');
+  //     });
 });
 
 // Template menu Helper
 Template.menu.helpers({
-    menus: function() {
-        return {
-            'current': 'index',
-            'left': [{
-                'active': true,
-                'link': '/search',
-                'text': 'Search'
-            }, {
-                'active': false,
-                'link': '/',
-                'text': 'Nav 2'
-            }, {
-                'active': false,
-                'link': '/',
-                'text': 'Nav 3'
-            }, {
-                'active': false,
-                'link': '/',
-                'text': 'Nav 4'
-            }, {
-                'active': false,
-                'link': '/',
-                'text': 'Nav 5'
-            }],
-            'rightLink': '\/session\/index',
-            'rightText': 'Log In'
-        };
-    }
+  menus: function() {
+    return {
+      'current': 'index',
+      'left': [{
+        'active': true,
+        'link': '/search',
+        'text': 'Search'
+      }, {
+        'active': false,
+        'link': '/',
+        'text': 'Nav 2'
+      }, {
+        'active': false,
+        'link': '/',
+        'text': 'Nav 3'
+      }, {
+        'active': false,
+        'link': '/',
+        'text': 'Nav 4'
+      }, {
+        'active': false,
+        'link': '/',
+        'text': 'Nav 5'
+      }],
+      'rightLink': '\/session\/index',
+      'rightText': 'Log In'
+    };
+  }
 });
 
 // JSON - display from Django
-Template.jsonArticle.rendered = function () {
-    console.log('[main] rendered');
-  };
+Template.jsonArticle.rendered = function() {
+  console.log('[main] rendered');
+};
 
-  Template.jsonArticle.helpers({
-    jsondata: function () { 
-      console.log("[main] 'test' helper executed");
-      return Session.get("jsondata");
-    }
-  });
+Template.jsonArticle.helpers({
+  jsondata: function() {
+    console.log("[main] 'test' helper executed");
+    return Session.get("jsondata");
+  }
+});
 
 
 // Search highlight
 Template.registerHelper('highlight', function(text) {
   var hashtagPattern = /\s*(#\w*)/gi,
     link = "/search/",
-    m, match, matches = [], t, url ='';
+    m, match, matches = [],
+    t, url = '';
 
   // initial check for hashtag in text
-  if(text.indexOf("#") !== -1) {
+  if (text.indexOf("#") !== -1) {
 
-      // find all #keywords (that have hashtags)
-      while ( (match = hashtagPattern.exec(text)) ) {
-        matches.push(match[0]);
-      }
-
-      // replace any #keywords with <a href="/search/keywords">#keywords</a>
-      for(var j=0; j < matches.length; j++) {
-        m = matches[j].replace(/\s/g, "");
-        // console.log('match',m);
-        url = link+m;
-        url = url.replace('#',"").toLowerCase(); // remove hashtag for lookup
-        t = " <a class='hashtag' href='"+url+"'>"+m+"</a> "; // replace with
-        replace = new RegExp("\\s*("+m+")", 'gi');
-
-        text = text.replace(replace, t);
-      }
+    // find all #keywords (that have hashtags)
+    while ((match = hashtagPattern.exec(text))) {
+      matches.push(match[0]);
     }
+
+    // replace any #keywords with <a href="/search/keywords">#keywords</a>
+    for (var j = 0; j < matches.length; j++) {
+      m = matches[j].replace(/\s/g, "");
+      // console.log('match',m);
+      url = link + m;
+      url = url.replace('#', "").toLowerCase(); // remove hashtag for lookup
+      t = " <a class='hashtag' href='" + url + "'>" + m + "</a> "; // replace with
+      replace = new RegExp("\\s*(" + m + ")", 'gi');
+
+      text = text.replace(replace, t);
+    }
+  }
   return text;
 });
--- meteor-articles/server/meteor-main.js
+++ meteor-articles/server/meteor-main.js
@@ -1,19 +1,20 @@
 Meteor.startup(function() {
 
   var apiBaseURL = "http://127.0.0.1:5000";
+  Future = Npm.require('fibers/future');
 
   Articles = new Mongo.Collection("articles");
   Articles.initEasySearch(['title', 'id'], {
-    'use' : 'mongo-db'
+    'use': 'mongo-db'
   });
-  
-  console.log("only on server")
+
+  console.log("DB Sync: ")
 
   Meteor.http.get(apiBaseURL + "/articles/?format=json", function(error, result) {
     if (error) {
       console.log('http get FAILED!');
     } else {
-      console.log('http get SUCCES');
+      console.log('http get SUCCESS');
       if (result.statusCode === 200) {
         console.log('Status code = 200!');
         //console.log(result.content);
@@ -30,10 +31,38 @@ Meteor.startup(function() {
 
       }
     }
+
+
+    Meteor.methods({
+      getArticleJSON: function(article_id) {
+        console.log('on server, getArticleJSON called with id: ', article_id);
+        var future = new Future();
+        if (article_id == undefined || article_id <= 0) {
+          throw new Meteor.Error(404, "invalid id");
+        }
+
+        Meteor.http.get(apiBaseURL + "/articles/" + article_id + "/?format=json", function(error, result) {
+          if (error) {
+            console.log('http get FAILED!');
+          } else {
+            console.log('http get SUCCESS');
+            if (result.statusCode === 200) {
+              future["return"](result)
+            }
+          }
+        });
+
+        return future.wait();
+        //return "Welcome: " + article_id;
+      }
+    });
+
+
+
   });
 
 
-  Meteor.publish("articles", function () {
+  Meteor.publish("articles", function() {
     return Articles.find({});
   });
 
