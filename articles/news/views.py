de2ad47fbeae9272768ee48ee54769da050e9ed6
Vishnu Ratheesh <vishnu@levitum.com>
2015-06-16 16:18:18 +0530

    Minor fix to display articles with permissible pub_date

--- articles/news/views.py
+++ articles/news/views.py
@@ -3,6 +3,7 @@ from .models import Category, Article
 from .serializers import CategorySerializer, ArticleSerializer
 from rest_framework import viewsets
 from rest_framework import generics
+from django.utils import timezone
 
 class CategoryViewSet(viewsets.ModelViewSet):
   """
@@ -15,7 +16,7 @@ class ArticleViewSet(viewsets.ModelViewSet):
   """
   API endpoint that allows categories to be viewed or edited.
   """
-  queryset = Article.objects.all()
+  queryset = Article.objects.filter(pub_date__lte=timezone.now())
   serializer_class = ArticleSerializer
 
 class ArticleList(generics.ListAPIView):
