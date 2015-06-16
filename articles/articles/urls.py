e39d994418cb20a81dcccb8a0b115198de171595
Vishnu Ratheesh <vishnu@levitum.com>
2015-06-16 15:36:17 +0530

    articles weren't syncing properly.

--- articles/articles/urls.py
+++ articles/articles/urls.py
@@ -41,6 +41,6 @@ urlpatterns = [
     url(r'^', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^admin/', include(admin.site.urls)),
-    url('^articles/(?P<articleid>.+)/$', views.ArticleList),
+    url(r'^articles/(?P<articleid>.+)/$', views.ArticleList),
     url(r'^search/', include('haystack.urls')),
 ]
--- meteor-articles/server/meteor-main.js
+++ meteor-articles/server/meteor-main.js
@@ -8,60 +8,74 @@ Meteor.startup(function() {
     'use': 'mongo-db'
   });
 
-  console.log("DB Sync: ")
 
-  Meteor.http.get(apiBaseURL + "/articles/?format=json", function(error, result) {
-    if (error) {
-      console.log('http get FAILED!');
-    } else {
-      console.log('http get SUCCESS');
-      if (result.statusCode === 200) {
-        console.log('Status code = 200!');
-        //console.log(result.content);
-        //Articles.remove({});
 
-        var res = JSON.parse(result.content)
-        res.results.forEach(function(article) {
-          // Articles.remove({
-          //   id: article.id
-          // });
-          //Articles.insert(article);
-          console.log("Inserted: " + article.id);
-        });
-
+  Meteor.methods({
+    getArticleJSON: function(article_id) {
+      console.log('on server, getArticleJSON called with id: ', article_id);
+      var future = new Future();
+      if (article_id == undefined || article_id <= 0) {
+        throw new Meteor.Error(404, "invalid id");
       }
-    }
+
+      Meteor.http.get(apiBaseURL + "/articles/" + article_id + "/?format=json", function(error, result) {
+        if (error) {
+          console.log('http get FAILED!');
+          future["return"](error.response)
+        } else {
+          console.log('http get SUCCESS');
+          if (result.statusCode === 200) {
+            future["return"](result)
+          }
+        }
+      });
+
+      return future.wait();
+      //return "Welcome: " + article_id;
+    },
+    dbSync: function() {
+      console.log("DB Sync: ")
+
+      Meteor.http.get(apiBaseURL + "/articles/?format=json", function(error, result) {
+        if (error) {
+          console.log('http get FAILED!');
+        } else {
+          console.log('http get SUCCESS');
+          if (result.statusCode === 200) {
+            console.log('Status code = 200!');
+            //console.log(result.content);
+            //Articles.remove({});
+
+            var res = JSON.parse(result.content)
+            res.results.forEach(function(article) {
 
 
-    Meteor.methods({
-      getArticleJSON: function(article_id) {
-        console.log('on server, getArticleJSON called with id: ', article_id);
-        var future = new Future();
-        if (article_id == undefined || article_id <= 0) {
-          throw new Meteor.Error(404, "invalid id");
+              rs = Articles.find({
+                id: article.id
+              });
+              if (rs.fetch().length) {
+                //console.log();
+                console.log('Matching article for: ' + article.id);
+
+                Articles.remove({
+                  _id: rs.fetch()[0]._id
+                });
+              } else {
+                console.log('No matching article for: ' + article.id);
+              }
+              Articles.insert(article);
+              console.log("Inserted: " + article.id);
+
+            });
+
+          }
         }
 
-        Meteor.http.get(apiBaseURL + "/articles/" + article_id + "/?format=json", function(error, result) {
-          if (error) {
-            console.log('http get FAILED!');
-            future["return"](error.response)
-          } else {
-            console.log('http get SUCCESS');
-            if (result.statusCode === 200) {
-              future["return"](result)
-            }
-          }
-        });
-
-        return future.wait();
-        //return "Welcome: " + article_id;
-      }
-    });
-
-
-
+      });
+    }
   });
 
+  Meteor.call('dbSync');
 
   Meteor.publish("articles", function() {
     return Articles.find({});
